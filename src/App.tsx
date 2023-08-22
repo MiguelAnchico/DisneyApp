3e7a68473e6c4f313ba56e214e282270828e55fa
MiguelAnchico <mangel.anchico@gmail.com>
2023-08-21 22:58:05 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+const App: React.FC = () => {
+  return (
+    <div className="grid place-items-center h-screen">
+      <h1 className="py-2">Hola</h1>
+      <p>Esto es un parrafo</p>
+    </div>
+  );
+};
+
+export default App;
